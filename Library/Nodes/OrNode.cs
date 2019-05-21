0763a3a215a5ed8c8296c960d61585c08925a6bd
bpbmoerk <bpbmoerk@avans.nl>
2019-05-21 11:51:50 +0200

    Added and and or nodes

---
+++ Library/Nodes/OrNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace DP1.Library.Nodes
+{
+    public class OrNode : NodeBase
+    {
+        /// <inheritdoc />
+        public OrNode(string nodeId)
+            : base(nodeId)
+        {
+        }
+
+        /// <inheritdoc />
+        public override void Calculate(params State[] input)
+        {
+            if (!input.Any())
+            {
+                throw new Exception("An OrNode must contain at least 1 inputs");
+            }
+
+            
+            this.CurrentState = new State
+            {
+                LogicState = input.Where(x => x.LogicState == true).Any()
+            };
+        }
+    }
+}
--- Tests/NodeTests.cs
+++ Tests/NodeTests.cs
@@ -26,5 +26,81 @@ namespace DP1.Tests
             inputNode.Calculate();
             Assert.IsTrue(inputNode.CurrentState.LogicState);
         }
+
+        [TestMethod]
+        public void AndNodeTest()
+        {
+            var andNode = new AndNode("");
+
+            var states = new List<State>
+            {
+                new State(true),
+                new State(true)
+            };
+
+            andNode.Calculate(states.ToArray());
+
+            Assert.IsTrue(andNode.CurrentState.LogicState);
+
+
+
+            states = new List<State>
+            {
+                new State(false),
+                new State(true)
+            };
+
+            andNode.Calculate(states.ToArray());
+
+            Assert.IsFalse(andNode.CurrentState.LogicState);
+
+
+
+            states = new List<State>
+            {
+                new State(false),
+                new State(false)
+            };
+
+            andNode.Calculate(states.ToArray());
+
+            Assert.IsFalse(andNode.CurrentState.LogicState);
+        }
+
+        [TestMethod]
+        public void OrNodeTest()
+        {
+            var orNode = new OrNode("");
+
+            var states = new List<State>
+            {
+                new State(true),
+                new State(true)
+            };
+
+            orNode.Calculate(states.ToArray());
+
+            Assert.IsTrue(orNode.CurrentState.LogicState);
+
+            states = new List<State>
+            {
+                new State(false),
+                new State(true)
+            };
+
+            orNode.Calculate(states.ToArray());
+
+            Assert.IsTrue(orNode.CurrentState.LogicState);
+
+            states = new List<State>
+            {
+                new State(false),
+                new State(false)
+            };
+
+            orNode.Calculate(states.ToArray());
+
+            Assert.IsFalse(orNode.CurrentState.LogicState);
+        }
     }
 }

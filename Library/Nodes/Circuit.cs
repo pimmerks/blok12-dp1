b5eb0acb4e1ac3066a4d064d016316215237c002
Pim Merks <merks.pim@gmail.com>
2020-06-14 19:52:52 +0200

    Removed unused projects

--- Library/Nodes/Circuit.cs
+++ Library/Nodes/Circuit.cs
@@ -1,21 +1,33 @@
-﻿using DP1.Library.Nodes;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DP1.Library.Nodes
+﻿namespace DP1.Library.Nodes
 {
-    // public class Circuit : NodeBase
-    // {
-    //     /// <inheritdoc />
-    //     public Circuit(string nodeId) : base(nodeId)
-    //     {
-    //     }
-    //
-    //     /// <inheritdoc />
-    //     public override void Calculate(params State[] input)
-    //     {
-    //         throw new NotImplementedException($"Circuit is not implemented yet. Id: {this.NodeId}");
-    //     }
-    // }
+    using DP1.Library.Nodes;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using DP1.Library.Factories;
+
+    public class Circuit : NodeBase
+    {
+        /// <inheritdoc />
+        public Circuit(string nodeId)
+            : base(nodeId)
+        {
+        }
+
+        public override void Register(NodeFactory factory)
+        {
+            // A circuit doesn't need to register.
+        }
+
+        /// <inheritdoc />
+        public override void Calculate(params State[] input)
+        {
+            throw new NotImplementedException($"Circuit is not implemented yet. Id: {this.NodeId}");
+        }
+
+        public override NodeBase Clone(string newId)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
